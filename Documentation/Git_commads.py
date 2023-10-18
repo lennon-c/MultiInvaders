8557433f717aeb72c840e3e06366c9193112c6c5
Carolina Lennon <14179052+lennon-c@users.noreply.github.com>
2023-10-18 14:57:45 +0200

    Add Git_commads to .gitignore

--- Documentation/Git_commads.py
+++ Documentation/Git_commads.py
@@ -7,13 +7,13 @@ Created on Wed Oct 18 12:43:50 2023
 
 
 #%% Initiate git
-# open Gitbash in game folder and write commands
+# open Gitbash in game folder and write the command
 
 """
 git init
 """
 
-#%% confi
+#%% confi 
 
 print('git config --global user.email "14179052+lennon-c@users.noreply.github.com"')
 print('git config --global user.name "Carolina Lennon"')
@@ -27,6 +27,7 @@ ignore=[
         'UserSetting.json',
         'GameSharing/',
         '__pycache__/'
+        , 'Git_commads.py'
         ]
 
 # set game folder as working directory in python
@@ -35,3 +36,30 @@ with open('.gitignore ', 'w') as f :
         f.write(i + "\n") 
  
     
+#%% Start tracking 
+"""
+git add *
+git add  .gitignore
+"""
+#%% First commit 
+def commit(comment, file = None):
+    """Commit commad with commit message"""
+    file_to_commit = file if file != None else  ''
+    print(f'git commit {file_to_commit} -m "{comment}"' )
+
+
+commit('Start')   
+"""
+git commit  -m "Start"
+"""
+
+#%% further commits
+
+def commit_a(comment, file = None):
+    """Commit commad with commit message and skip stage area"""
+    file_to_commit = file if file != None else  ''
+    print(f'git commit {file_to_commit} -a -m "{comment}"' ) 
+    
+    
+commit_a('Add Git_commads to .gitignore')
+# git commit  -a -m "Add Git_commads to .gitignore"
